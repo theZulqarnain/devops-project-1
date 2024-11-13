862dc844eacd9253412482b5ed893a5a9e461a1d
Abdulla Zulqarnain <zul@Abdullas-MacBook-Pro.local>
2024-11-13 13:25:15 +0530

    buckets commented

--- infra/remote_backend_s3.tf
+++ infra/remote_backend_s3.tf
@@ -1,7 +1,7 @@
-terraform {
-  backend "s3" {
-    bucket = "dev-proj-1-remote-state-bucket-12345"
-    key    = "devops-project-1/terraform.tfstate"
-    region = "ap-south-1"
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket = "dev-proj-1-remote-state-bucket-12345"
+#     key    = "devops-project-1/terraform.tfstate"
+#     region = "ap-south-1"
+#   }
+# }
